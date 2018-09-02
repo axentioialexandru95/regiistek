e3c8883f7fc353fbd61d8b193489bcf297bbdcd4
Axentioi Alexander <axentioialexandru95@gmail.com>
2018-09-02 17:05:45 +0300

    Added frontend implementation without responsiveness

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    var sidebar = $('#sidebar-menu')
+    var smallSidebar = $('#small-navbar')
+    var content = $('.content')
+    var closeSidebar = $('#close-sidebar')
+    var sidebarList = $('#sidebar-list')
+    var contentOverlay = $('#navbar-overlay')
+
+    smallSidebar.on('click', function(){
+        sidebar.fadeIn(400)
+        contentOverlay.fadeIn(800)
+    })
+
+    sidebarList.children().on('click', function(el){
+        content.fadeOut()
+        var url = el.currentTarget.childNodes[1].href
+        window.location = url
+    })
+
+    closeSidebar.on('click', function(){
+        sidebar.fadeOut(400);
+        content.css('padding-left', '0')
+        contentOverlay.fadeOut(200)
+    })
+    
+    if(sidebar.css('display') != 'none'){
+        content.css('padding-left', sidebar.css('width'))
+    } else {
+        content.css('padding-left', '0')
+    }
+    
+    $('#preloader').fadeOut()
+    setTimeout(function(){
+        $('.flex-row').fadeIn()
+    }, 1000)
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+For this website the structure will be:
+
+1. Home
+2. About
+3. Services
+5. Careers
+6. Contact
+
+
+Main layout:
+
+- Left sidebar menu
+- Social footer
+- Info in right
